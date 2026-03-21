c6e2d9a097cfaa35548e435521734e94cd97d5b9
gustavocarvalho472-a1ly <gustavocarvalho472-a1ly@users.noreply.github.com>
2026-03-21 11:38:44 -0300

    chore: update base path for cvxox2 repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/cxvox/',
+  base: '/cvxox2/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
